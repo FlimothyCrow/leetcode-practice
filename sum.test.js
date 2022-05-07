84273933f1e3ac405418e9e0ce9facb553541f31
FlimothyCrow <timshogren@gmail.com>
2022-05-07 09:07:34 -0600

    nesting test blocks

--- sum.test.js
+++ sum.test.js
@@ -1,9 +1,24 @@
 const sum = require("./sum")
 
-test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3)
+// describes can nest n deep
+
+describe("sum test block", () => {
+    test("adds 1 + 2 to equal 3", () => {
+        expect(sum(1, 2)).toBe(3)
+    })
+
+    test("adds 2 + 2 to equal 4", () => {
+        expect(sum(2, 2)).toBe(4)
+    })
 })
 
-test("adds 2 + 2 to equal 4", () => {
-    expect(sum(2, 2)).toBe(4)
+describe("addition test block", () => {
+    test("adds 1 + 2 to equal 3", () => {
+        expect(sum(1, 2)).toBe(3)
+    })
+
+    test("adds 2 + 2 to equal 4", () => {
+        expect(sum(2, 2)).toBe(4)
+    })
 })
+
