1420d3135111cae8ba754a0c2d5b9ca968760a76
FlimothyCrow <timshogren@gmail.com>
2024-01-24 17:50:32 -0700

    deleted check.test.js, added global modifer to vitest install for describe blocks

--- sum.js
+++ sum.js
@@ -1,25 +1,6 @@
 module.exports = {
     isPalindrome,
-}
-
-function reverseString(str) {
-    str = str.split("")
-    let length = str.length
-    for (var i = 0; i < str.length / 2; i++) {
-        let last = str[length - i] // "c" > "abc"
-        str[length - i] = str[i] // str[2] = "a" > "aba"
-        str[i] = last // "c" > "cba"
-    }
-    return str.join("")
-}
-
-function reverseStringOutOfPlace(str) {
-    let orderedString = ""
-    for (var i = 0; i < str.length; i++) {
-        orderedString = str[i] + orderedString
-        //orderedString = replaceCharAtIdx(str, str[i], i)
-    }
-    return orderedString
+    mockingCase,
 }
 
 function mockingCase(str) {
@@ -34,51 +15,7 @@ function mockingCase(str) {
         }
     })
     return accumString
-}
-
-// normal recursive solutions use two functions
-
-// add arrayOfNums[0] and accum, that becomes newAccum
-// remove (shift) arrayOfNums[0]
-// if arrayOfNums has length, return recursiveSum[shorterArray, newAccum]
-// else return accum
-
-function recurseFlatten(nestedArrays, accum) {
-    let toAdd = 0
-    if (nestedArrays[0]) {
-        if (!Array.isArray(nestedArrays[0])) {
-            toAdd = nestedArrays[0]
-            nestedArrays.shift()
-            return recurseFlatten(nestedArrays, accum + toAdd)
-        } else {
-            toAdd = nestedArrays[0] // [1, 2]
-            nestedArrays.shift()
-            return recurseFlatten(toAdd, accum)
-        }
-    } else {
-        return accum
-    }
-}
-
-function flatten(nestedArrays) {
-    return recurseFlatten(nestedArrays, 0)
-}
-
-function searchInsert(nums, target) {
-    for (var i = 0; i < nums.length; i++) {
-        if (nums[i] === target) {
-            return i
-        }
-        if (target < nums[0]) {
-            return 0
-        }
-        if (target > nums[i]) {
-            if (target <= nums[i + 1]) {
-                return i + 1
-            }
-        }
-    }
-    return nums.length
+    // abc returns aBc
 }
 
 // str.replace(/[^a-z0-9]/gi, '')
--- sum.test.js
+++ sum.test.js
@@ -1,18 +1,28 @@
-const { isPalindrome } = require("./sum")
-import { expect, test } from "vitest"
+const { isPalindrome, mockingCase } = require("./sum")
+import { it, describe, expect, test } from "vitest"
 
 // describes can nest n deep
 // .only || .skip
+describe("this method returns true if a string is a palindrome", () => {
+    test("abcba returns true", () => {
+        expect(isPalindrome("abcba")).toEqual(true)
+    })
+    test("aaab returns false", () => {
+        expect(isPalindrome("aaab")).toEqual(false)
+    })
+    test("A man, a plan, a canal: Panama returns true", () => {
+        expect(isPalindrome("A man, a plan, a canal: Panama")).toEqual(true)
+    })
+    test("race a car returns false", () => {
+        expect(isPalindrome("race a car")).toEqual(false)
+    })
+})
 
-test("abcba returns true", () => {
-    expect(isPalindrome("abcba")).toEqual(true)
-})
-test("aaab returns false", () => {
-    expect(isPalindrome("aaab")).toEqual(false)
-})
-test("A man, a plan, a canal: Panama returns true", () => {
-    expect(isPalindrome("A man, a plan, a canal: Panama")).toEqual(true)
-})
-test("race a car returns false", () => {
-    expect(isPalindrome("race a car")).toEqual(false)
+describe("this method alternates lower and upper case in a string", () => {
+    test("abc returns aBc", () => {
+        expect(mockingCase("abc")).toEqual("aBc")
+    })
+    test("aaa bbb  ccc should be aAa bBb  CcC", () => {
+        expect(mockingCase("aaa bbb  ccc")).toBe("aAa BbB  cCc")
+    })
 })
