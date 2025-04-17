ac09512b039547becc91dd527a62f56aa6a811cc
Timothy <timshogren@gmail.com>
2025-04-17 10:56:56 -0400

    defined longestPalindrome() and reconfigured isPalindrome() for strings only

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,58 +1,89 @@
 // sum.test.js
-import { describe, expect, test } from "vitest"
-import { returnFace, sum, mockingCase, removeDuplicates, differenceOfSums, isPalindrome } from "./sum"
+import { describe, expect, test } from "vitest";
+import {
+    returnFace,
+    sum,
+    mockingCase,
+    removeDuplicates,
+    differenceOfSums,
+    isPalindrome,
+    longestPalindrome,
+} from "./sum";
 
 test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3)
-})
+    expect(sum(1, 2)).toBe(3);
+});
 
 test("Card{ face: 5, suit: 'diamonds' } should return 5", () => {
-    expect(returnFace({ face: 5, suit: "diamonds" })).toBe(5)
-})
+    expect(returnFace({ face: 5, suit: "diamonds" })).toBe(5);
+});
 describe("mockingCase tests", () => {
     test("abc should be aBc", () => {
-        expect(mockingCase("abc")).toBe("aBc")
-    })
+        expect(mockingCase("abc")).toBe("aBc");
+    });
 
     test("abc def ghi should be aBc DeF gHi", () => {
-        expect(mockingCase("abc def ghi")).toBe("aBc DeF gHi")
-    })
+        expect(mockingCase("abc def ghi")).toBe("aBc DeF gHi");
+    });
 
     test("123", () => {
-        expect(mockingCase("123")).toBe("123")
-    })
-})
+        expect(mockingCase("123")).toBe("123");
+    });
+});
 describe("remove duplicates in place and return new length", () => {
     test("[1, 2, 3, 3] modifies in place to [1, 2, 3] and return length 3", () => {
-        expect(removeDuplicates([1, 2, 3, 3])).toBe(3)
-    })
+        expect(removeDuplicates([1, 2, 3, 3])).toBe(3);
+    });
 
     test("[5, 5, 6, 6, 6, 7] modifies in place to [5, 6, 7] and return length 3", () => {
-        expect(removeDuplicates([5, 5, 6, 6, 6, 7])).toBe(3)
-    })
-})
+        expect(removeDuplicates([5, 5, 6, 6, 6, 7])).toBe(3);
+    });
+});
 
 describe("returns the difference between m-divisible ints betwen 1-n and non-m-divisible ints between 1-m ", () => {
     test("[1, 2, 4, 5, 7, 8, 10] - [3, 6, 9] = 19", () => {
-        expect(differenceOfSums(10, 3)).toBe(19)
-    })
+        expect(differenceOfSums(10, 3)).toBe(19);
+    });
     test("[1, 2, 3, 4, 5] - [] = 15", () => {
-        expect(differenceOfSums(5, 6)).toBe(15)
-    })
+        expect(differenceOfSums(5, 6)).toBe(15);
+    });
+});
 
-})
+describe("returns true if a string is palindromic, else returns false", () => {
+    test("aba should return true", () => {
+        expect(isPalindrome("aba")).toBe(true);
+    });
+    test("aab should return false", () => {
+        expect(isPalindrome("aab")).toBe(false);
+    });
+    test("' cdc' should return false", () => {
+        expect(isPalindrome(" cdc")).toBe(false);
+    });
+    test("fkkf should return true", () => {
+        expect(isPalindrome("fkkf")).toBe(true);
+    });
+});
 
-describe("returns true if a number is palindromic, else returns false", () => {
-    test("979 should return true", () => {
-        expect(isPalindrome(979)).toBe(true)
-    })
-    test("456 should return false", () => {
-        expect(isPalindrome(456)).toBe(false)
-    })
-    test("456 should return false", () => {
-        expect(isPalindrome(-121)).toBe(false)
-    })
-    test("1221 should return true", () => {
-        expect(isPalindrome(1221)).toBe(true)
-    })
-})
+describe("returns longest palindromic substring", () => {
+    test("cbbd should return bb", () => {
+        expect(longestPalindrome("cbbd")).toBe("bb");
+    });
+    test("aaaaa should return aaaaa", () => {
+        expect(longestPalindrome("aaaaa")).toBe("aaaaa");
+    });
+    test("abc should return ''", () => {
+        expect(longestPalindrome("abcde")).toBe("a");
+    });
+    test("abcc should return cc", () => {
+        expect(longestPalindrome("abcc")).toBe("cc");
+    });
+    test("abc c should return ''", () => {
+        expect(longestPalindrome("abc c")).toBe("c c");
+    });
+    test("things sgniht should return things sgniht", () => {
+        expect(longestPalindrome("things sgniht")).toBe("things sgniht");
+    });
+    test("a should return a", () => {
+        expect(longestPalindrome("a")).toBe("a");
+    });
+});
--- src/sum.ts
+++ src/sum.ts
@@ -1,68 +1,110 @@
 // sum.js
 export function sum(a: number, b: number) {
     // return doesn't need to be typed, it's inferred from argument types
-    return a + b
+    return a + b;
 }
 
 export interface Card {
     // template for any objects named
-    face: number
-    suit: string
+    face: number;
+    suit: string;
 }
 
 export function returnFace(card: Card): number {
-    return card.face
+    return card.face;
 }
 
-
 export function mockingCase(initialStr: string) {
-    let accumString = ""
-    let shouldBeLowerCase = true
+    let accumString = "";
+    let shouldBeLowerCase = true;
     initialStr.split("").forEach((char) => {
         if (char === " ") {
-            accumString += char
+            accumString += char;
         } else {
-            shouldBeLowerCase ? (accumString += char.toLowerCase()) : (accumString += char.toUpperCase())
-            shouldBeLowerCase = !shouldBeLowerCase
+            shouldBeLowerCase
+                ? (accumString += char.toLowerCase())
+                : (accumString += char.toUpperCase());
+            shouldBeLowerCase = !shouldBeLowerCase;
         }
-    })
-    return accumString
+    });
+    return accumString;
     // abc returns aBc
 }
 
 export function removeDuplicates(nums: number[]) {
     for (var firstIdx = 0; firstIdx < nums.length; firstIdx++) {
-        let deleteCounter = 0
-        for (var secondIdx = firstIdx + 1; secondIdx < nums.length; secondIdx++) {
+        let deleteCounter = 0;
+        for (
+            var secondIdx = firstIdx + 1;
+            secondIdx < nums.length;
+            secondIdx++
+        ) {
             if (nums[firstIdx] == nums[secondIdx]) {
-                deleteCounter++
+                deleteCounter++;
             }
         }
-        nums.splice(firstIdx + 1, deleteCounter)
+        nums.splice(firstIdx + 1, deleteCounter);
     }
-    return nums.length
+    return nums.length;
     // could we return the finished splice? it returns final length, might skip the .length
 }
 
 export function differenceOfSums(n: number, m: number) {
-    let num1 = 0
-    let num2 = 0
+    let num1 = 0;
+    let num2 = 0;
     for (var i = 1; i <= n; i++) {
-        i % m == 0 ? (num1 += i) : (num2 += i)
+        i % m == 0 ? (num1 += i) : (num2 += i);
     }
-    return num2 - num1
+    return num2 - num1;
 }
 
-export function isPalindrome(x: number): boolean {
+export function isPalindrome(x: string): boolean {
+    const xHalfLength = Math.ceil(x.length / 2);
 
-    const xAsString = x.toString(); 
-    const xHalfLength = Math.ceil(xAsString.length / 2);
-    
     for (let i = 0; i < xHalfLength; i++) {
-        if (xAsString[i] != xAsString[xAsString.length - 1 - i]){
-            return false
+        if (x[i] != x[x.length - 1 - i]) {
+            return false;
         }
     }
 
-    return true
+    return true;
+}
+
+export function longestPalindrome(s: string): string {
+    if (s.length == 1) {
+        return s;
+    }
+
+    let longestSubstring = s[0];
+
+    // returns true if palindromic
+    const isPalindromeLocal = (subString: string): boolean => {
+        const xHalfLength = Math.ceil(subString.length / 2);
+
+        for (let i = 0; i < xHalfLength; i++) {
+            if (subString[i] != subString[subString.length - 1 - i]) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        for (let k = i + 1; k < s.length; k++) {
+            const currentSubstring = s.substring(i, k + 1);
+            // print substring
+            console.log(`\n current substring = ${currentSubstring}`);
+
+            // check if substring is palindrome
+            if (isPalindromeLocal(currentSubstring)) {
+                console.log(`\n ${currentSubstring} is a palindrome`);
+                if (currentSubstring.length > longestSubstring.length) {
+                    longestSubstring = currentSubstring;
+                }
+            }
+        }
+    }
+
+    return longestSubstring;
 }
